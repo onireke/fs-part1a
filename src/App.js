5fe2650f0aeb1414d43dfbd28a83244fdb8d51d1
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-06-11 12:42:34 +0100

    fs part1-step3

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import Total from "./components/Total";
 
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+  const course = 'Half Stack application development'
+   const part1 = { 
+     name:'Fundamentals of React',
+     exercises1 : 10
+    }
+
+  const part2 = {
+    name :'Using props to pass data',
+    exercises2 : 7
+
+  }
+
+
+  const part3 = {
+    name :'State of a component',
+    exercises3 : 14
+  }  
+  
+  
   return (
     <div className="App">
 
       <Header  course={course} />
-      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
-      <Total total={exercises1+exercises2+exercises3}/>
+      <Content part1={part1.name} exercise1={part1.exercises1} part2={part2.name} exercise2={part2.exercises2} part3={part3.name} exercise3={part3.exercises3} />
+      <Total total={part1.exercises1+ part2.exercises2+ part3.exercises3}/>
     </div>
   );
 }
